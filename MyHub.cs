4fc2fe723e6209472bc98dec26c884cd5f5d6c04
Muhammad Umar <82210932+Umar9780@users.noreply.github.com>
2023-12-22 17:00:55 +0500

    init
    comit

---
+++ MyHub.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoApp
+{
+    using DemoApp.Models;
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+
+    public class MyHub : Hub
+    {
+        public async Task SendMessage(User user)
+        {
+
+            await Clients.Client(user.Id.ToString()).SendAsync("Notification", user);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using DemoApp;
+using DemoApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
+builder.Services.AddDbContext<AppDbContext>(op=>op.UseSqlServer(builder.Configuration.GetConnectionString("Conn")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapHub<MyHub>("/myHub");
+app.Run();
